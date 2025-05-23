bbe1635d705a7ce0741b77e70799278f2d3acf01
simenspets <simen.s.storhov@gmail.com>
2025-05-23 14:02:59 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+ (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '67d2c9ff5a106f4d24f64037' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
